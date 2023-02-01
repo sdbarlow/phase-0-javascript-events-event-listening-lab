8b13c1b6407d7aa828730fbaf93d6ea51069d517
Seth Barlow <sethdbarlow@gmail.com>
2023-02-01 14:58:45 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => alert('I was clicked'));
 }
+
+const button = document.getElementById('button');
+button.addEventListener('click')
